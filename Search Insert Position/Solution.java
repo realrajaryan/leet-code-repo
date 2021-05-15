8b3672eccc973cbf915c262db9470908e7d74dad
Raj Aryan Singh <58954869+realrajaryan@users.noreply.github.com>
2021-05-15 22:09:00 +0530

    leetcode.com/problems/search-insert-position

---
+++ Search Insert Position/Solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        for(int i=0; i<nums.length; i++) {
+            if(nums[i] >= target) {
+                return i;
+            }
+            else if((nums[i] < target) && ((i == nums.length - 1) || (nums[i+1] > target))) {
+                return i+1;
+            }
+        }
+        return 0; //to avoid compiler error
+    }
+}
